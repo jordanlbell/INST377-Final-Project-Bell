632bb962928220b57f7ec3790a7db2ae9423ec0f
jordanlbell <jordanlbell19@gmail.com>
2023-04-28 13:33:06 -0400

    Final Project Reset, Fetching Data

--- script.js
+++ script.js
@@ -1,122 +1,47 @@
 
-function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector('#crime_list');
-  target.innerHTML = '';
-  list.forEach((item) => {
-    const str = `<li>${item.name}</li>`;
-    target.innerHTML += str
-
-  });
-}
-
-
-function crimeByYear(list) {
- 
-    return list
-
-
-}
-
-/* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+
   return list.filter((item) => {
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = query.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
   })
 
-}
-
 
 }
 
-function markerPlace(array, map) {
-
-  console.log('array for markers', array)
-
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item) => {
-    console.log('markerPlace', item);
-    const { coordinates } = item.geocoded_column_1
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
-
-  })
-
-}
-
-async function mainEvent() { // the async keyword means we can make API requests
+async function mainEvent() {
   const mainForm = document.querySelector('.main_form');
-  const loadDataButton = document.querySelector('#data_load');
-  const clearDataButton = document.querySelector('#data_clear');
-  const generateListButton = document.querySelector('#generate');
-  const textField = document.querySelector('#street_number')
+  const filterButton = document.querySelector('.filter_button');
 
-  const loadAnimation = document.querySelector('#data_load_animation');
-  loadAnimation.style.display = 'none';
-  generateListButton.classList.add('hidden');
-
-  const carto = initMap();
-
-  const storedData = localStorage.getItem('storedData');
-  let parsedData = JSON.parse(storedData);
-  if (parsedData?.length > 0) {
-    generateListButton.classList.remove('hidden');
-  }
-
-  let currentList = []; // this is "scoped" to the main event function
-
-  /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-
-    console.log('loading data');
-    loadAnimation.style.display = 'inline-block';
+  let currentList = [];
 
 
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  mainForm.addEventListener('submit', async (submitEvent) => {
 
-    const storedList = await results.json();
-    localStorage.setItem('storedData', JSON.stringify(storedList));
-    parsedData = storedList
-    if (parsedData?.length > 0) {
-      generateListButton.classList.remove('hidden');
-    }
-    loadAnimation.style.display = 'none';
-    // console.table(storedList);
+    submitEvent.preventDefault();
+    console.log('form submission');
 
 
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
+
+    const currentList = await results.json();
+
+
+    console.log(currentList)
   });
 
-  generateListButton.addEventListener('click', (event) => {
-    console.log('generate new list')
-    currentList = crimeByYear(parsedData);
-    console.log(currentList);
-    injectHTML(currentList);
-    markerPlace(currentList, carto);
-  })
-
-  textField.addEventListener('input', (event) => {
-    console.log('input', event.target.value);
-    const newList = filterList(currentList, event.target.value);
-    console.log(newList)
-    injectHTML(newList);
-    markerPlace(newList, carto);
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton')
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.street_number);
+    console.log(newList);
   })
 
 
-  clearDataButton.addEventListener("click", (event) => {
-    console.log('clear browser data');
-    localStorage.clear
-    console.log('localStorage Check', localStorage.getItem("storedData"))
-  })
 }
 
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
-
-
