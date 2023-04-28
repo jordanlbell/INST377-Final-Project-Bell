172d1bfdb098a44322850072b019f0a18f830c0f
jordanlbell <jordanlbell19@gmail.com>
2023-04-28 13:32:42 -0400

    Final Project Reset, Loading Data

--- script.js
+++ script.js
@@ -28,13 +28,6 @@ function filterList(list, query) {
 
 }
 
-function initMap() {
-  const carto = L.map('map').setView([38.98, -76.93], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(carto);
-  return carto;
 
 }
 
