e596095a39cdb649182109d7877779d208bc0ef4
jordanlbell <jordanlbell19@gmail.com>
2023-05-03 16:02:06 -0400

    Map, markerPlace

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ function injectHTML(list) {
 function filterList(list, query) {
   console.log(list)
   return list.filter((item) => {
-  //  console.log("street_number", item.street_number)
-  //  console.log("typeOf", typeof item.street_number)
+
     if (typeof item.street_number != "undefined"){
 
       const lowerCaseName = item.street_number.toLowerCase();
@@ -24,7 +23,40 @@ function filterList(list, query) {
 
     }
   })
+ 
 
+}
+
+function initMap() {
+  const carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+
+}
+
+function markerPlace(array, map) {
+
+  console.log('array for markers', array)
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log('markerPlace', item);
+    const locationPoint = {
+      type: "Point",
+      coordinates: [item.longitude, item.latitude]
+    };
+    const { coordinates } = locationPoint
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+
+  })
 
 }
 
@@ -32,7 +64,10 @@ async function mainEvent() {
   const mainForm = document.querySelector('.main_form');
   const filterButton = document.querySelector('.filter_button');
   
-  
+  const carto = initMap();
+
+  const storedData = localStorage.getItem('storedData');
+
   let currentList = [];
 
 
@@ -44,22 +79,29 @@ async function mainEvent() {
 
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
 
-    currentList = await results.json();
+    storedList = await results.json();
+    localStorage.setItem('storedData', JSON.stringify(storedList));
 
-
-    console.log(currentList)
+    console.log(storedList)
   });
 
   filterButton.addEventListener('click', (event) => {
     console.log('clicked FilterButton')
+    currentList = storedList
     const formData = new FormData(mainForm);
     const formProps = Object.fromEntries(formData);
     console.log(formProps);
     const newList = filterList(currentList, formProps.street);
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
   })
 
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear
+    console.log('localStorage Check', localStorage.getItem("storedData"))
+  })
 
 }
 
